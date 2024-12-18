d1f70b1b2944d0b21f8ea881c4aaeae731345991
shaheerzkhan <szkhan@sunbonn.com>
2024-12-18 18:09:38 +0500

    2nd commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
+
+const Welcome = () => {
+  const router = useRouter();
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Shaheer</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput}></TextInput>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
